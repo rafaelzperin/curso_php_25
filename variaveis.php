6aeecd3c4aeac5352a9e9b49551d98491e636ab5
rafaelzperin <rafael-perin@hotmail.com>
2024-11-26 21:36:38 -0300

    variáveis

---
+++ variaveis.php
@@ -0,0 +1,20 @@
+<?php
+const CPF = '04185875070';
+const VELOCIDADE_DA_LUZ = '320 m/s2' // escrever em letras maiúsculas e underline
+//tipos de dados primitivos do PHP: declarar variávies
+$inteiro = 2;
+$string = "um texto qualquer";
+$nomePessoa = "Rafael Perin"; //style guide camel case
+$nome_pessoa = "Rafael Perin"; // style guide snake case
+$float = 2.5;
+$double = 2.5468;
+$char = 'a';
+
+
+// tipos de dados estruturais
+$array_short = [];
+$array = array( );
+
+$objeto = new stdClass(); // objeto dinâmico
+$classe = new stdClass(); // 
+
