868ea6c6cc96c3e8206560822656c27418dbd8e9
Rafael Zanovelo Perin <rafael-perin@hotmail.com>
2024-12-14 08:55:27 -0300

    Update primos.php

--- primos.php
+++ primos.php
@@ -1,4 +1,12 @@
 <?php
 //Exercício 2: Dizer se o número é primo ou não.
 $numero = 5;
-$restoPar = $numero % 2;
+//$restoPar = $numero % 2;
+ for ($i = 0; $i <= ($numero - 1); $i++) {
+  $resto = $numero % $i;
+   if {
+     $resto == 0
+       echo "<br> %numero não é primo!"
+       break
+   }
+ }
