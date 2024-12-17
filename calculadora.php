a3134fb3665c69abd30de1d6e351b1cb634a3281
rafaelzperin <rafael-perin@hotmail.com>
2024-12-17 20:32:14 -0300

    calculadora

--- calculadora.php
+++ calculadora.php
@@ -6,20 +6,17 @@
 echo "1 - adição <br> 2 - subtração <br> 3 - multiplicação <br> 4 - divisão";
 $numero1=3;
 $numero2=1;
-$operador= "+";
+$operador= "*";
 //x=$numero1 $operador $numero2;
 
 if ($operador == "+"){
     $x = $numero1 + $numero2;
-} elseif ($operador == 2){
+} elseif ($operador == "-"){
     $x= $numero1 - $numero2;
-} elseif ($operador == 3) {
+} elseif ($operador == "*") {
     $x= $numero1 * $numero2;
-} elseif ($operador == 4) {
+} else ($operador == "/") {
     $x= $numero1 / $numero2;
-} else {
-    echo "escolha outro operador!";
-    exit;
 }
 
-echo "<br> o resultado é: $x";
+echo "<br> $numero1 $operador $numero2 =  $x";
