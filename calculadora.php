7550d6f8d858c80d5dd3262ca654408c8c33d45f
rafaelzperin <rafael-perin@hotmail.com>
2024-12-17 21:04:15 -0300

    calculadora

--- calculadora.php
+++ calculadora.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * operadores: +, -, *, /;
+ * operadores: +, -, *, /, %;
  * 
  */
 echo "<br> CALCULADORA <br>";
 $numero1=3;
-$numero2=0;
+$numero2=5;
 $operador= "/";
 
 if ($operador == "+"){
@@ -16,12 +16,18 @@ if ($operador == "+"){
     $resultado= $numero1 * $numero2;
 } elseif ($operador == "/" && $numero2 == 0) {
     echo "ERRO: divisão por zero";
-    exit;
 } 
 elseif ($operador == "/") {
     $resultado= $numero1 / $numero2;
-} else {
+} elseif ($operador == "#"){
+    $resultado= $numero2 * 100 / $numero1;
+}
+else {
     echo " <br> ERRO <br>";
 }
-
-echo " <br> $numero1 $operador $numero2 =  $resultado <br>";
+    if ($operador == "#"){
+    echo "$numero2 é $resultado %";
+}
+    else{
+        echo " <br> $numero1 $operador $numero2 =  $resultado <br>";
+    }
