ffc3f02171c26e0fc119c35d40fdd00a34dc4a07
rafaelzperin <rafael-perin@hotmail.com>
2024-12-17 21:05:18 -0300

    calculadora

--- calculadora.php
+++ calculadora.php
@@ -26,7 +26,7 @@ else {
     echo " <br> ERRO <br>";
 }
     if ($operador == "#"){
-    echo "$numero2 é $resultado %";
+    echo "$numero2 é $resultado % de $numero1";
 }
     else{
         echo " <br> $numero1 $operador $numero2 =  $resultado <br>";
