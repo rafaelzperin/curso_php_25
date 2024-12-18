0e201a5f9c0dd7dcc6697510036727642e4abe0a
rafaelzperin <rafael-perin@hotmail.com>
2024-12-17 21:46:43 -0300

    triangulo

--- triangulos.php
+++ triangulos.php
@@ -1,20 +1,21 @@
 <?php
-echo "<br> CONDIÇÃO DE EXISTÊNCIA DE UM TRIÂNGULO: <br> ";
-$ladoA = 2;
+echo "<br> CLASSIFICAÇÃO DE UM TRIÂNGULO: <br> ";
+$ladoA = 20;
 $ladoB = 4;
 $ladoC = 3;
 
-if ($ladoA + $ladoB > $ladoC && $ladoA + $ladoC > $ladoB && $ladoB + $ladoC > $ladoA ){
-    if ($ladoA == $ladoB && $ladoA == $ladoC && $ladoB == $ladoC ) {
-        echo " <br> o triângulo é equilátero! <br>";
-    } elseif ($ladoA == $ladoB || $ladoA == $ladoC || $ladoB == $ladoC){
-        echo " <br> o triângulo é isósceles! <br>";
-    } else{
-        echo " <br> o triângulo é escaleno! <br>";
-    }
-} else{ 
+$isTriangulo = ($ladoA + $ladoB > $ladoC && $ladoA + $ladoC > $ladoB && $ladoB + $ladoC > $ladoA);
+
+
+if ( ! $isTriangulo ){ // NEGAÇÃO ! 
     echo " <br> não é triângulo <br>";
     exit;
 }
 
-
+if ($ladoA == $ladoB && $ladoA == $ladoC && $ladoB == $ladoC ) {
+    echo " <br> o triângulo é equilátero! <br>";
+} elseif ($ladoA == $ladoB || $ladoA == $ladoC || $ladoB == $ladoC){
+    echo " <br> o triângulo é isósceles! <br>";
+} else{
+    echo " <br> o triângulo é escaleno! <br>";
+}
