b76f6ee4b61471ce7061545a7cb58427d8c8eb03
Rafael Zanovelo Perin <rafael-perin@hotmail.com>
2025-01-07 19:43:52 -0300

    Update calendário.php

--- calendário.php
+++ calendário.php
@@ -16,7 +16,7 @@ $mes = substr($data, 3 , 2);
 $ano = substr($data, 6 , 4);
 $isdia = ($dia >= 1 && $dia <= 31);
 $ismes = ($mes >= 1 && $dia <= 12);
-$isano = ($ano > 0);
+$isano = ($ano > 1582);
 
 if (! $isdia){
 echo " <br> o dia está incorreto <br>";
