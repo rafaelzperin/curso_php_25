f8075d9ae32ac2bb9f58c2030c8a11d1ea6e4fda
Rafael Zanovelo Perin <rafael-perin@hotmail.com>
2025-01-07 10:46:39 -0300

    Update calendário.php

--- calendário.php
+++ calendário.php
@@ -1,11 +1,38 @@
-<?php
 /** 
  * criar um algoritmo que simula o calendário.
  * variáveis: $data = "01/01/2025"
  * validar a data
  * se é bissexto;
  * $data= 31/12/2024;
+ *        0123456789 - posições
  * %dia = substr($data, 0 , 2)
  * %mes = substr($data, 3 , 2)
  * %ano = substr($data, 6 , 4)
  */
+<?php
+$data = "01/01/2025";
+$dia = substr($data, 0 , 2);
+$mes = substr($data, 3 , 2);
+$ano = substr($data, 6 , 4);
+$isdia = ($dia >= 1 && $dia <= 31);
+$ismes = ($mes >= 1 && $dia <= 31);
+$isano = ($ano > 0);
+
+if (! $isdia){
+echo " <br> o dia está incorreto <br>";
+} elseif (! $ismes){
+echo " <br> o mês está incorreto <br>";
+} else (! $isano){
+echo " <br> o ano está incorreto <br>";
+}
+
+$centenario = $ano % 100;
+$iscentenario = ($centenario == 0);
+$resto = $ano % 4;
+  if($iscentenario % 400 == 0){
+    echo "<br> o ano é bissexto! <br>";
+  } elseif (! $iscentenario | $resto == 0){
+    echo "<br> o ano é bissexto! <br>";
+  } else {
+    echo "<br> o ano não é bissexto! <br>";
+  }
