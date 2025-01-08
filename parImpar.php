c969de588c71c8cabf2e697ca70df1524326107f
rafaelzperin <rafael-perin@hotmail.com>
2025-01-07 21:31:12 -0300

    pares e impares

--- parImpar.php
+++ parImpar.php
@@ -5,21 +5,30 @@
  * e vamos  informar se é par ou ímpar
  */
 
- $numero = 3020923939239;
- $resto = $numero % 2;
- if ($resto == 0){
-    echo "<br> O número $numero é <b> par <b> ! <br>";
- } else{
-    echo "<br> O número $numero é <b> ímpar <b>! <br>";
- }
+//  $numero = 3020923939239;
+//  $resto = $numero % 2;
+
+//  if ($resto == 0){
+//     echo "<br> O número $numero é <b> par <b> ! <br>";
+//  } else{
+//     echo "<br> O número $numero é <b> ímpar <b>! <br>";
+//  }
 
  // Quais são os 10 primeiros pares e ímpares?
- echo " <br> par ... ímpar <br>";
+ 
  $quantidade = 10;
+
+ $pares = [];
+ $impares = [];
+
  for ($i = 0; $i <= $quantidade; $i++) { 
+
    $par = 2 * $i;
-   $impar = 2* $i + 1;
-   echo " <br> $par ... $impar <br>";
-//   $pares = array[$par];
+   $impar = 2 * $i + 1;
+
+   $pares[]=$par;
+   $impares[]=$impar;
 }
-//echo "$pares";
+
+echo " <br> Pares: " .implode(", ", $pares) . "<br>"; 
+echo " <br> Ímpares: " .implode(", ", $impares) . "<br>";
