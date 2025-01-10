982dd92adea6f56b21df5bcab34c7d6028b56227
Rafael Zanovelo Perin <rafael-perin@hotmail.com>
2025-01-10 11:56:07 -0300

    Update ordenarNumeros.php

--- ordenarNumeros.php
+++ ordenarNumeros.php
@@ -3,16 +3,20 @@
 
 $numeros = [10, 5, 2, 30, 85, 14];
 $tamanho = count($numeros);
-//$tamanho - 1
-for ($i = 1; $i = 2; $i++){
+
+for ($j = 1; $j <= $tamanho - 1; $j++){
+    for ($i = 1; $i <= $tamanho - 1; $i++){
         if ($numeros[$i-1] > $numeros[$i]){
+           
             $a = $numeros[$i-1];
             $b = $numeros[$i];
+            
             $numeros[$i-1] = $b;
             $numeros[$i] = $a;
+        // echo " <br> Números: " .implode(", ", $numeros) . "<br>";
         } else{
         }
     }
-echo $ $numeros $;
-//echo " <br> Primos: " .implode(", ", $numeros) . "<br>"; 
-    
+}
+
+ echo " <br> Números: " .implode(", ", $numeros) . "<br>";
