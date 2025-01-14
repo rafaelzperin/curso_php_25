9aefb66d53cdad917e2cf8c921b531112004c7ba
rafaelzperin <rafael-perin@hotmail.com>
2025-01-14 20:38:01 -0300

    update

--- tabuada.php
+++ tabuada.php
@@ -1,16 +1,15 @@
 <?php
 $numero = 5; // tabuada do número
 $limite = 10; // quando a tabuada para
- for ($i = 0; $i <= $limite; $i++) { 
-    $resultado = $numero * $i;
-    echo " <br> $i x $numero = $resultado <br>";
- }
 
-  function tabuada($numero,$limite){
+  function tabuada($numero, $limite = 10) {
    for ($i = 0; $i <= $limite; $i++) { 
       $resultado = $numero * $i;
       echo " <br> $i x $numero = $resultado <br>";
    }
  }
 
- tabuada($numero=6, $limite=12);
+ $numero = 6;
+ $limite = 12;
+ 
+ tabuada($numero, $limite);
