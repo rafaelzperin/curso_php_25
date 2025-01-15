1e3646680979001365be09e30dce59bd96863edc
rafaelzperin <rafael-perin@hotmail.com>
2025-01-14 21:38:48 -0300

    update

--- ordenarNumeros.php
+++ ordenarNumeros.php
@@ -3,7 +3,7 @@
 
 $numeros = [84, 3, 10, 5, 2, 30, 85, 14, 6];
 $tamanho = count($numeros);
-$k = 0;
+// $k = 0;
 for ($j = 1; $j <= $tamanho - 1; $j++){
     $numerosAnt = $numeros; 
     for ($i = 1; $i <= $tamanho - 1; $i++){
@@ -23,7 +23,7 @@ for ($j = 1; $j <= $tamanho - 1; $j++){
     } else {
 
     }
-    $k++;
+    // $k++;
 }
-echo " $k";
+//echo " $k"; número de iterações
  echo " <br> Números: " .implode(", ", $numeros) . "<br>";
